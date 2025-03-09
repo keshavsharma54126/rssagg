38d54152ce0aac952bb15e08372289f753385f07
Keshav sharma <sharmakeshav54126@gmail.com>
2025-03-09 14:13:49 +0000

    api for adding user

--- handler_user.go
+++ handler_user.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/keshavsharma54126/rssagg/internal/database"
 )
 
-func(apiCfg *apiConfig)handlerCreateUser(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, 200, struct{}{})
+func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Name string `json:"name"`
+	}
+	decoder := json.NewDecoder((r.Body))
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error parsing JSON:%v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Could not create user: %v", err))
+	}
+
+	respondWithJSON(w, 200, dataBaseUserToUser(user))
 }
